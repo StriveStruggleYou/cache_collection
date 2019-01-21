382d00175f4cf8ff662cc5321bae105735f8dade
StriveStruggleYou <StriveStruggleYou@163.com>
2019-01-21 20:55:28 +0800

    新增

--- cache_collections/src/main/java/io/github/ssy/cache/web/RedisController.java
+++ cache_collections/src/main/java/io/github/ssy/cache/web/RedisController.java
@@ -1,10 +1,9 @@
 package io.github.ssy.cache.web;
 
 
-import io.github.ssy.cache.redis.CacheService;
+import io.github.ssy.cache.service.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
