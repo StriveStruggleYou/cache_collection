8a98c8336930f9d945e34c24dea7a40ce45dce3a
StriveStruggleYou <StriveStruggleYou@163.com>
2019-01-29 14:48:45 +0800

    基于容量回收

--- guava_cache/src/main/java/io/github/ssy/Guava.java
+++ guava_cache/src/main/java/io/github/ssy/Guava.java
@@ -46,6 +46,8 @@ public class Guava {
    * 基于容量回收的时候，还可以设置一个权重，就可以搞定缓存的整个大小了。
    *
    * code = 1基于容量的key 数量回收 code = 2基于容量的key 权重回收
+   *
+   * code = 3基于时间多少秒没有get 访问回收 code =4 基于时间 多少秒没有覆盖写 回收,一般会组合使用和容量不会单独使用
    */
   Cache buildCache(int code) {
     Cache<Integer, String> cache = null;
@@ -76,6 +78,27 @@ public class Guava {
           .expireAfterWrite(10, TimeUnit.MINUTES)
           //构建cache实例
           .build();
+    } else if (code == 3) {
+      cache = CacheBuilder.newBuilder()
+          //设置cache的初始大小为10，要合理设置该值
+          .initialCapacity(10)
+          //设置并发数为5，即同一时间最多只能有5个线程往cache执行写入操作
+          .concurrencyLevel(5)
+          //设置cache中的数据在写入之后的存活时间为10分钟
+          .expireAfterAccess(10, TimeUnit.MINUTES)
+          //构建cache实例
+          .build();
+    } else if (code == 4) {
+      cache = CacheBuilder.newBuilder()
+          //设置cache的初始大小为10，要合理设置该值
+          .initialCapacity(10)
+          //设置并发数为5，即同一时间最多只能有5个线程往cache执行写入操作
+          .concurrencyLevel(5)
+          //设置cache中的数据在写入之后的存活时间为10分钟
+          .expireAfterWrite(10, TimeUnit.MINUTES)
+          //构建cache实例
+          .build();
+
     }
 
     return cache;
